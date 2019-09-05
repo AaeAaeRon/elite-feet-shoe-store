2c0c525d5efdd630493153781cb838d38b8d70d9
Aaron Briscoe <abriscoe2008gmail.com>
2019-09-05 17:14:26 -0500

    d

--- frontend/src/components/ShoeCard.js
+++ frontend/src/components/ShoeCard.js
@@ -3,9 +3,14 @@ import React from 'react'
 export default class ShoeCard extends React.Component {
     render() {
         return(
-            <div>
-                <h1>{this.props.all.name}</h1>
-            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h3 class="card-text">{this.props.all.name}</h3>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div> 
         )
     }
 }
--- frontend/src/components/ShoeContainer.js
+++ frontend/src/components/ShoeContainer.js
@@ -5,6 +5,7 @@ export default class ShoeContainer extends React.Component {
     render() {
         return(
             <div>
+            
                 {this.props.displayShoes.map(all => <ShoeCard all={all}/>)}
             </div>
         )
