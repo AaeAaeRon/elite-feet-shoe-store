54de8a205a5806352ebd7da136a9675364dcb932
Sfimadi94 <sydeffect1994@gmail.com>
2019-09-05 16:16:59 -0500

    routes working

--- backend/app/controllers/selections_controller.rb
+++ backend/app/controllers/selections_controller.rb
@@ -1,2 +1,12 @@
 class SelectionsController < ApplicationController
+
+    def index
+        @selections = Selection.all
+        render json: @selections, only: [:id, :shoe_id, :order_id ]
+    end
+
+    def show
+        @selection = Selection.find(params[:id])
+        render json: @selection, only: [:id, :shoe_id, :order_id]
+    end
 end
--- backend/app/controllers/shoes_controller.rb
+++ backend/app/controllers/shoes_controller.rb
@@ -1,2 +1,19 @@
 class ShoesController < ApplicationController
+
+    # before_action:current_shoe, only:[:show]
+    def index
+        @shoes = Shoe.all
+        render json: @shoes, only: [:id, :img_url ,:name, :price, :category]
+
+
+    end
+
+
+    def show
+        @shoe = Shoe.find(params[:id])
+        render json: @shoe, only: [:id, :img_url ,:name, :price, :category]
+        
+    end
+
+  
 end
