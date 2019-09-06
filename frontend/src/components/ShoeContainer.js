7a62061a0d28d1170b6ca4d4776bf3a7cbe4e059
Sfimadi94 <sydeffect1994@gmail.com>
2019-09-06 14:15:03 -0500

    css

--- frontend/src/components/ShoeContainer.js
+++ frontend/src/components/ShoeContainer.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import ShoeCard from './ShoeCard'
 
+// import './App.css';
+// import {Container, Row, Col} from 'react-bootstrap'
+ 
 export default class ShoeContainer extends React.Component {
     render() {
         return(
-            <div className="container-fluid d-flex justify-content-center">
-                <div className="col-xl-12 col-lg-12 col-md-12 col-xs-12">
+        <div class="container">
+            {/* <div className="container-fluid d-flex justify-content-center"> */}
+            {/* <div className="col-xl-12 col-lg-12 col-md-12 col-xs-12"> */}
 
 
-                <div className="row">
-                    <div className="col-xl-3 col-lg-3 col-md-3 col-xs-3">                
-                    {this.props.displayShoes.map(all => <ShoeCard all={all}/>)}
-                    </div>
-                </div>
-                </div>
-             </div>
+            <div className="row">
+                        
+                {this.props.displayShoes.map(all => <ShoeCard all={all}/>)}
+        </div>
+        {/* </div> */}
+    {/* </div> */}
+    </div>
+            
         )
     }
-}
+}   
+
+// <div className="container-fluid d-flex justify-content-center">
+// <div className="col-xl-12 col-lg-12 col-md-12 col-xs-12">
+
+
+// <div className="row">
+//     <div className="col-xl-3 col-lg-3 col-md-3 col-xs-3">                
+//     {this.props.displayShoes.map(all => <ShoeCard all={all}/>)}
+//     </div>
+// </div>
+// </div>
+// </div>
